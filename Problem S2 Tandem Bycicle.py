12a70cc340ea5d375b68757289699bcd7f13592f
Leon Dai <linchuandai@gmail.com>
2017-01-27 19:17:23 -0500

    Update Problem S2 Tandem Bycicle.py

--- Problem S2 Tandem Bycicle.py
+++ Problem S2 Tandem Bycicle.py
@@ -1,35 +1,21 @@
 #CCC Senior 2016 Exam Problem 2 Tandem Bicycle
 
-import math
-
-#Function to find lowest speed
-def lowestSpeed (Dmojistan_list,Pegland_list, number_citizens):
+def findSpeed (Dmojistan_list,Pegland_list, number_citizens, question_number):
 
     speedSum = 0; 
-    Dmojistan_list.sort()
-    Pegland_list.sort()
+    Dmojistan_list.sort(key=int)
 
-    for i in range (0, math.ceil(number_citizens/2)+1):
-        if Dmojistan_list [i] > Pegland_list [i]:
+    if question_number == 1:
+        Pegland_list.sort(key=int)
+    else:
+        Pegland_list.sort(key=int,reverse=True)
+
+    for i in range (0, number_citizens):
+        if int(Dmojistan_list [i]) > int(Pegland_list [i]):
             speedSum += int (Dmojistan_list [i])
         else:
             speedSum += int (Pegland_list [i])
-             
-    return speedSum
-
-#Function to find highest speed
-def highestSpeed (Dmojistan_list,Pegland_list, number_citizens):
-
-    speedSum = 0;
-    Dmojistan_list.sort()
-    Pegland_list.sort(reverse=True)
-
-    for i in range (0, math.ceil(number_citizens/2)+1):
-        if Dmojistan_list [i] > Pegland_list [i]:
-            speedSum += int (Dmojistan_list [i])
-        else:
-            speedSum += int (Pegland_list [i])
-             
+            
     return speedSum
 
 #inputs from user   
@@ -41,7 +27,4 @@ Pegland_citizens = input ()
 Dmojistan_list = Dmojistan_citizens.split (' ')
 Pegland_list = Pegland_citizens.split (' ')
 
-if question_number == 1:
-    print (lowestSpeed (Dmojistan_list, Pegland_list, number_citizens))
-elif question_number == 2:
-    print (highestSpeed (Dmojistan_list, Pegland_list, number_citizens))
+print (findSpeed (Dmojistan_list, Pegland_list, number_citizens, question_number))
